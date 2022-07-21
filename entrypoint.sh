05912cef89ae951f7bdb6388c7696fc31f0f692b
Monika <81293197+mon-pan@users.noreply.github.com>
2022-07-21 10:52:17 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -192,10 +192,15 @@ else
     echo "I got to here!!" 
     curl \
       -X PUT \
-      -H "Accept: application/vnd.github.v3+json" \
-      -u ${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN} \
-      "https://api.github.com/repos/${INPUT_REPOSITORY}/pulls/${PR_NUMBER}/merge" \
-      -d '{"Merging PR for ":"${INPUT_SOURCE_BRANCH}"}'
+      -H "Accept: application/vnd.github+json" \ 
+      -H "Authorization: ${GITHUB_TOKEN}" \
+      https://api.github.com/repos/${INPUT_REPOSITORY}/pulls/${PR_NUMBER}/merge
+#     curl \
+#       -X PUT \
+#       -H "Accept: application/vnd.github.v3+json" \
+#       -u ${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN} \
+#       "https://api.github.com/repos/${INPUT_REPOSITORY}/pulls/${PR_NUMBER}/merge" \
+#       -d '{"Merging PR for ":"${INPUT_SOURCE_BRANCH}"}'
   fi
   # Pass in other cases
   echo -e "\n[INFO] No errors found."

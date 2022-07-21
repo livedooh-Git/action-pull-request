1c19f86dbeea0cd0573e3e5a3a150f31373cc417
Monika <81293197+mon-pan@users.noreply.github.com>
2022-07-21 11:20:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -190,7 +190,7 @@ else
     # Auto-merge PR if target branch is develop
   if [[ "${INPUT_TARGET_BRANCH}" ==  "develop" ]]; then
     echo "I got to here!!" 
-    hub api --method PUT "repos/${INPUT_REPOSITORY}/pulls/${PR_NUMBER}/merge"
+    hub api --method POST "repos/${INPUT_REPOSITORY}/pulls/${PR_NUMBER}/merge"
 #     curl \
 #       -X PUT \
 #       -H "Accept: application/vnd.github.v3+json" \
